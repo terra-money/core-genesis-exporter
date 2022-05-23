118e7c4f38e48abfd359a786ab4a423e4c69ddea
jess <jesse@soob.co>
2022-05-24 02:24:10 +0900

    feat(app/export): STEAK

---
+++ app/export/steak/contract_export_steak.go
@@ -0,0 +1,50 @@
+package steak
+
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/terra-money/core/app"
+	"github.com/terra-money/core/app/export/util"
+	wasmtypes "github.com/terra-money/core/x/wasm/types"
+)
+
+var (
+	AddressSteakHub   = "terra15qr8ev2c0a0jswjtfrhfaj5ucgkhjd7la2shlg"
+	AddressSteakToken = "terra1rl4zyexjphwgx6v3ytyljkkc4mrje2pyznaclv"
+)
+
+func ExportSteak(app *app.TerraApp, bl *util.Blacklist) (util.SnapshotBalance, error) {
+	// Blacklist steak hub from LUNA balance snapshot
+	bl.RegisterAddress(util.DenomLUNA, AddressSteakHub)
+
+	ctx := util.PrepCtx(app)
+	qs := util.PrepWasmQueryServer(app)
+
+	// 1. Iterate over all Steak token holders, get their balance
+	var balanceMap = make(map[string]sdk.Int)
+	if err := util.GetCW20AccountsAndbalances(ctx, app.WasmKeeper, AddressSteakToken, balanceMap); err != nil {
+		return nil, fmt.Errorf("error during cw20 iteration: %v", err)
+	}
+
+	// 2. Get Steak<>LUNA Exchange Rate
+	var hubState struct {
+		ExchangeRate sdk.Dec `json:"exchange_rate"`
+	}
+	if err := util.ContractQuery(ctx, qs, &wasmtypes.QueryContractStoreRequest{
+		ContractAddress: AddressSteakHub,
+		QueryMsg:        []byte("{\"state\":{}}"),
+	}, &hubState); err != nil {
+		return nil, fmt.Errorf("failed to query SteakHub state: %v", err)
+	}
+
+	// 3. Iterate over balanceMap and apply exchange rate
+	var finalBalance = make(util.SnapshotBalance)
+	for addr, bal := range balanceMap {
+		finalBalance[addr] = util.Balance{
+			Denom:   util.DenomLUNA,
+			Balance: hubState.ExchangeRate.MulInt(bal).TruncateInt(),
+		}
+	}
+
+	return finalBalance, nil
+}
