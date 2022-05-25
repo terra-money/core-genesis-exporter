ba77a2c5a77c4bb2645f78cdeb36185eae0cbefd
Jason Stallings <jason@stallin.gs>
2022-05-25 02:19:27 -0500

    fix: Finish Stader pools.

--- app/export/stader/contract_export_stader_pools.go
+++ app/export/stader/contract_export_stader_pools.go
@@ -2,7 +2,6 @@ package stader
 
 import (
 	"fmt"
-	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	terra "github.com/terra-money/core/app"
@@ -14,7 +13,6 @@ import (
 const (
 	StaderPools      = "terra1r2vv8cyt0scyxymktyfuudqs3lgtypk72w6m3m"
 	StaderDelegator  = "terra1t9ree3ftvgr70fvm6y67zsqxjms8jju8kwcsdu"
-	StaderLunaX      = "terra17y9qkl8dfkeg4py7n0g5407emqnemc3yqk5rup"
 	StaderController = "terra1xacqx447msqp46qmv8k2sq6v5jh9fdj37az898"
 	StaderSCC        = "terra127vwnwgwdvq94ce4ws76ddh0c699jt40dznrn2"
 )
@@ -26,34 +24,40 @@ func ExportStaderPools(app *terra.TerraApp, bl *util.Blacklist) (util.SnapshotBa
 	// Pull users from user_registry map.
 	// pub const USER_REGISTRY: Map<(&Addr, U64Key), UserPoolInfo> = Map::new("user_registry");
 	prefix := util.GeneratePrefix("user_registry")
-	delegatorAddr, err := sdk.AccAddressFromBech32(StaderPools)
+	delegatorAddr, err := sdk.AccAddressFromBech32(StaderDelegator)
 	if err != nil {
 		return nil, err
 	}
 
 	users := []string{}
 	app.WasmKeeper.IterateContractStateWithPrefix(sdk.UnwrapSDKContext(ctx), delegatorAddr, prefix, func(key, value []byte) bool {
-		// First character in key is a comma.
-		stakerAddr := sdk.AccAddress(strings.TrimLeft(string(key), ","))
-
-		users = append(users, stakerAddr.String())
+		// Filter out characters from start and end of the key.
+		correctedAddress := string(key)[2:46]
+		if !contains(users, correctedAddress) {
+			users = append(users, correctedAddress)
+		}
 		return false
 	})
 
 	balances := make(util.SnapshotBalanceMap)
 	for _, address := range users {
-		// fmt.Println(address)
+		previousAmount := balances[address].Balance
+		if previousAmount.IsNil() {
+			previousAmount = sdk.NewInt(0)
+		}
 
-		for i := 1; i < 3; i++ {
+		for i := 0; i < 3; i++ {
 			var poolUserInfo struct {
 				Info struct {
-					Deposit struct {
+					Deposit *struct {
 						Staked sdk.Int `json:"staked"`
-					} `json:"deposit"`
+					} `json:"deposit,omitempty"`
+					Undelegations []struct {
+						Amount sdk.Int `json:"amount"`
+					} `json:"undelegations"`
 				} `json:"info"`
 			}
 
-			// TODO: Figure out why none of the addresses in the user_registry have a balance.
 			if err := util.ContractQuery(ctx, q, &wasmtypes.QueryContractStoreRequest{
 				ContractAddress: StaderPools,
 				QueryMsg:        []byte(fmt.Sprintf("{\"get_user_computed_info\": {\"user_addr\": \"%s\", \"pool_id\": %d}}", address, i)),
@@ -61,25 +65,56 @@ func ExportStaderPools(app *terra.TerraApp, bl *util.Blacklist) (util.SnapshotBa
 				panic(err)
 			}
 
-			fmt.Println(poolUserInfo)
+			if poolUserInfo.Info.Deposit != nil {
+				previousAmount = previousAmount.Add(poolUserInfo.Info.Deposit.Staked)
+			}
+
+			for _, undelegation := range poolUserInfo.Info.Undelegations {
+				if !undelegation.Amount.IsZero() {
+					previousAmount = previousAmount.Add(undelegation.Amount)
+				}
+			}
+		}
+
+		// Fetch unclaimed rewards for users.
+		var sccUserInfo struct {
+			User struct {
+				RetainedRewards  sdk.Int `json:"retained_rewards"`
+				UserStrategyInfo []struct {
+					TotalRewards sdk.Int `json:"total_rewards"`
+				} `json:"user_strategy_info"`
+				Undelegations []struct {
+					Amount sdk.Int `json:"amount"`
+				} `json:"undelegation_records"`
+			} `json:"user"`
+		}
+
+		if err := util.ContractQuery(ctx, q, &wasmtypes.QueryContractStoreRequest{
+			ContractAddress: StaderSCC,
+			QueryMsg:        []byte(fmt.Sprintf("{\"get_user\": {\"user\": \"%s\"}}", address)),
+		}, &sccUserInfo); err != nil {
+			panic(err)
+		}
+
+		previousAmount = previousAmount.Add(sccUserInfo.User.RetainedRewards)
+
+		if len(sccUserInfo.User.UserStrategyInfo) > 0 {
+			previousAmount = previousAmount.Add(sccUserInfo.User.UserStrategyInfo[0].TotalRewards)
+		}
+
+		for _, undelegation := range sccUserInfo.User.Undelegations {
+			previousAmount = previousAmount.Add(undelegation.Amount)
+		}
+
+		if !previousAmount.IsZero() {
+			balances[address] = util.SnapshotBalance{
+				Denom:   util.DenomLUNA,
+				Balance: previousAmount,
+			}
 		}
 	}
 
-	// get LunaX <> LUNA ER
-	var staderState struct {
-		State struct {
-			ExchangeRate sdk.Dec `json:"exchange_rate"`
-		} `json:"state"`
-	}
-	if err := util.ContractQuery(ctx, q, &wasmtypes.QueryContractStoreRequest{
-		ContractAddress: StaderController,
-		QueryMsg:        []byte("{\"state\":{}}"),
-	}, &staderState); err != nil {
-		return nil, err
-	}
-
-	// TODO: call get_user on SCC to cover pending rewards.
-
-	bl.RegisterAddress(util.DenomLUNA, StaderPools)
+	// TODO: Figure out proper address to blacklist.
+	// bl.RegisterAddress(util.DenomLUNA, StaderPools)
 	return balances, nil
 }
