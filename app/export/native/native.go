7d4e68e4828274de22774834c1ee3a3053e139aa
javier <javier.su@terra.money>
2022-05-27 12:01:34 +0000

    update get native balance

--- app/export/native/native.go
+++ app/export/native/native.go
@@ -1,8 +1,6 @@
 package native
 
 import (
-	"fmt"
-
 	"github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	terra "github.com/terra-money/core/app"
@@ -12,37 +10,36 @@ import (
 func ExportAllBondedLuna(app *terra.TerraApp) (util.SnapshotBalanceAggregateMap, error) {
 	ctx := util.PrepCtx(app)
 	uCtx := types.UnwrapSDKContext(ctx)
+
+	validators := app.StakingKeeper.GetAllValidators(uCtx)
+	valMap := make(map[string]stakingtypes.Validator)
+	for _, v := range validators {
+		valMap[v.OperatorAddress] = v
+	}
+
 	var unbondingDelegations []stakingtypes.UnbondingDelegation
 	app.StakingKeeper.IterateUnbondingDelegations(uCtx, func(_ int64, ubd stakingtypes.UnbondingDelegation) (stop bool) {
 		unbondingDelegations = append(unbondingDelegations, ubd)
 		return false
 	})
 
-	var redelegations []stakingtypes.Redelegation
-	app.StakingKeeper.IterateRedelegations(uCtx, func(_ int64, red stakingtypes.Redelegation) (stop bool) {
-		redelegations = append(redelegations, red)
-		return false
-	})
-
-	bondedDelegations := app.StakingKeeper.GetAllDelegations(uCtx)
-	validators := app.StakingKeeper.GetAllValidators(uCtx)
-
-	valMap := make(map[string]stakingtypes.Validator)
-	for _, v := range validators {
-		valMap[v.OperatorAddress] = v
-	}
-
+	c := 0
 	snapshot := make(util.SnapshotBalanceAggregateMap)
-	for _, del := range bondedDelegations {
+	app.StakingKeeper.IterateAllDelegations(uCtx, func(del stakingtypes.Delegation) (stop bool) {
+		c += 1
+		if c%10000 == 0 {
+			app.Logger().Info(".... %d", c)
+		}
 		v, ok := valMap[del.ValidatorAddress]
 		if !ok {
-			return nil, fmt.Errorf("validator not found %s", del.ValidatorAddress)
+			return false
 		}
 		snapshot.AppendOrAddBalance(del.DelegatorAddress, util.SnapshotBalance{
 			Denom:   util.DenomLUNA,
 			Balance: v.TokensFromShares(del.Shares).TruncateInt(),
 		})
-	}
+		return false
+	})
 
 	for _, ub := range unbondingDelegations {
 		for _, entry := range ub.Entries {
